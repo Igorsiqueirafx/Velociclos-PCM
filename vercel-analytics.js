bf83db0c5301c2bf07ff1ed8b8861484d18315f8
Igor Siqueira <igorlemefx@gmail.com>
2026-03-14 22:39:59 -0300

    fix: use inject() method for Vercel Analytics and Speed Insights

---
+++ vercel-analytics.js
@@ -0,0 +1,7 @@
+// Vercel Analytics
+import { inject } from "@vercel/analytics";
+inject();
+
+// Vercel Speed Insights
+import { injectSpeedInsights } from "@vercel/speed-insights";
+injectSpeedInsights();
